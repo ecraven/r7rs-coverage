08d5ccea1f02e76b6db390a55833c7d2020490e2
Peter <craven@gmx.net>
2016-09-21 13:03:28 +0200

    more checks

--- input/r7rs/vectors.scm
+++ input/r7rs/vectors.scm
@@ -31,3 +31,23 @@
 (assert (equal? #(#\f #\o #\o) (string->vector "xfoo" 1)))
 ;; TEST
 (assert (equal? #(#\f #\o #\o) (string->vector "xfoox" 1 4)))
+;; TEST
+(assert (let ((v #(1 2 3))) (and (equal? v (vector-copy v)) (not (eq? v (vector-copy v))))))
+;; TEST
+(assert (let ((v #(5 1 2 3))) (equal? #(1 2 3) (vector-copy v 1))))
+;; TEST
+(assert (let ((v #(5 1 2 3 5))) (equal? #(1 2 3) (vector-copy v 1 4))))
+;; TEST
+(define v (make-vector 3)) (vector-copy! v 0 #(1 2 3)) (assert (equal? #(1 2 3) v))
+;; TEST
+(define v (make-vector 3 #\x)) (vector-copy! v 0 #(5 1 2 3) 1) (assert (equal? #(1 2 3) v))
+;; TEST
+(define v (make-vector 3 #\x)) (vector-copy! v 0 #(5 1 2 3 4) 1 4) (assert (equal? #(1 2 3) v))
+;; TEST
+(assert (equal? #(1 2 3 4) (vector-append #(1 2) #(3 4))))
+;; TEST
+(define v (make-vector 3 0)) (vector-fill! v 1) (assert (equal? #(1 1 1) v))
+;; TEST
+(define v (make-vector 3 0)) (vector-fill! v 1 1) (assert (equal? #(0 1 1) v))
+;; TEST
+(define v (make-vector 3 0)) (vector-fill! v 1 1 2) (assert (equal? #(0 1 0) v))
