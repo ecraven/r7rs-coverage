61708e6f5eedf9947245119130f61028b0375663
Peter <craven@gmx.net>
2018-03-23 21:02:37 +0100

    Fix typo

--- input/scheme-time.scm
+++ input/scheme-time.scm
@@ -1,6 +1,6 @@
 ;; TEST current-jiffy
 (let ((t (current-jiffy))) (assert (number? t)) (assert (exact? t)) (assert (positive? t)) (assert (integer? t)))
-;; TEST current-secondlist
+;; TEST current-second
 (let ((t (current-second))) (assert (number? t)) (assert (inexact? t)) (assert (< 1505000000 t 1735686000))) ;; smaller 2025
 ;; TEST jiffies-per-second
 (let ((t (jiffies-per-second))) (assert (number? t)) (assert (exact? t)) (assert (integer? t)))
