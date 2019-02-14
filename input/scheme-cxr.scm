3df63bd0eb91cadb074fb16f95ced128fb64ae8a
Peter <craven@gmx.net>
2019-02-14 21:02:56 +0100

    Add some tests to cxr.

--- input/scheme-cxr.scm
+++ input/scheme-cxr.scm
@@ -1,3 +1,19 @@
+;; TEST caaar
+(assert (= 2 (caaar '(((2))))))
+;; TEST caadr
+(assert (= 2 (caadr '(3 . ((2))))))
+;; TEST cadar
+(assert (= 2 (cadar '((3 . (2))))))
+;; TEST caddr
+(assert (= 2 (caddr '(3 . (3 . (2))))))
+;; TEST cdaar
+(assert (= 2 (cdaar '(((3 . 2))))))
+;; TEST cdadr
+(assert (= 2 (cdadr '(3 . ((3 . 2))))))
+;; TEST cddar
+(assert (= 2 (cddar '((3 . (3 . 2))))))
+;; TEST cdddr
+(assert (= 2 (cdddr '(3 . (3 . (3 . 2))))))
 ;; TEST cXr
 (list caaar
       caadr
